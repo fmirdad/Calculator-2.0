5674fc0e0b5242af96dcc9be98032be58ae55920
Frank Mirdad <fmirdad1@gmail.com>
2025-10-24 16:06:16 +0100

    Added functionality to split the equation string into multiple parts, using a space as the delimiter. Created variables for each item in the list split adn assigned the number values and converted them to integers and housed the operator in its own variable.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            equationBox = new TextBox();
+            label1 = new Label();
+            Calculate = new Button();
+            resultBox = new TextBox();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // equationBox
+            // 
+            equationBox.Location = new Point(328, 94);
+            equationBox.Name = "equationBox";
+            equationBox.Size = new Size(191, 23);
+            equationBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label1.Location = new Point(196, 97);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Please input equation";
+            // 
+            // Calculate
+            // 
+            Calculate.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            Calculate.Location = new Point(328, 152);
+            Calculate.Name = "Calculate";
+            Calculate.Size = new Size(191, 27);
+            Calculate.TabIndex = 2;
+            Calculate.Text = "Calculate";
+            Calculate.UseVisualStyleBackColor = true;
+            Calculate.Click += Calculate_Click;
+            // 
+            // resultBox
+            // 
+            resultBox.Location = new Point(328, 215);
+            resultBox.Name = "resultBox";
+            resultBox.Size = new Size(191, 23);
+            resultBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label2.Location = new Point(280, 218);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Result";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(resultBox);
+            Controls.Add(Calculate);
+            Controls.Add(label1);
+            Controls.Add(equationBox);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox equationBox;
+        private Label label1;
+        private Button Calculate;
+        private TextBox resultBox;
+        private Label label2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,20 @@ namespace Calculator_2._0
         {
             InitializeComponent();
         }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            string equation = equationBox.Text; // declared the text from equationBox as a variable "equation"
+            char[] delimiter = { ' ' }; // sets a space as the delimiter to split the equation
+            string[] split = equation.Split(delimiter); // splits the equation using the above delimiter
+
+            int num1 = Convert.ToInt32(split[0]); // using split[0] convert the first number to int
+            int num2 = Convert.ToInt32(split[2]); // using split[2] convert the second number to int
+            string op = split[1]; // using split[1] find the operator used by the user
+
+
+
+
+        }
     }
 }
